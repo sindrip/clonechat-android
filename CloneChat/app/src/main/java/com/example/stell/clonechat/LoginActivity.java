b873772b388758a6fb211c1932af79aa82a65ed8
stellarutg <stellarut95@gmail.com>
2018-02-13 21:56:27 +0000

    green accent color replaced

--- CloneChat/app/src/main/java/com/example/stell/clonechat/LoginActivity.java
+++ CloneChat/app/src/main/java/com/example/stell/clonechat/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.stell.clonechat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
